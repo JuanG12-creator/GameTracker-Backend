11f9215522ff56fc208b062e597a9ecaf007fd6b
Juan Jose <juanguzman0811@gmail.com>
2025-11-16 13:45:07 -0500

    eliminacion de autorizaciones y mildware

--- routes/autorizacion.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const autorizacionController = require("../controllers/autorizacion.controller.js");
-
-router.post("/registro", autorizacionController.registro);
-router.post("/login", autorizacionController.login);
-
-module.exports = router;
